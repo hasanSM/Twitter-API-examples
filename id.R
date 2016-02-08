b76afa63eb1bd82aac696fb188f2f5e9c97e87fa
hasanSM <hasan@predictify.me>
2016-02-08 10:40:25 +0500

    Update id.R

--- id.R
+++ id.R
@@ -1,3 +1,7 @@
+  #This requires the httr and the jsonlite packages. First we setup the Authorization for our commands.
+  #For this you need to enter your Consumer/API Key, Consumer/API Secret, Token and Token secret in the specified places below.
+myapp = oauth_app("twitter", key="yourConsumerKeyHere",secret="yourConsumerSecretHere")
+sig = sign_oauth1.0(myapp, token = "yourTokenHere", token_secret = "yourTokenSecretHere")
   #We can use the unique id of a Tweet to locate and retrieve it.
   #In the following example the id of the Tweet is 692765099879063552
   #and we look to see how many Retweets it has and the details of who Retweeted it.
